4565ed7fd0d6cf1bdd9277d54a952764e56170c4
gustavorodi <gustavo.rodrigues@catolicasc.org.br>
2020-10-27 19:43:54 -0300

    teste

--- Test/executeTest.rb
+++ Test/executeTest.rb
@@ -4,35 +4,35 @@ require_relative '../ops/soma'
 require_relative '../ops/multiplicar'
 
 class ExecuteTest
-    def startTest()
-      puts "Teste da validação (As variáveis são numeros?): " + self.testValid().to_s
-      puts "Teste soma passou ? " + self.testSoma().to_s
-      puts "Teste Divisão passou ? " + self.testDivisao().to_s
-      puts "Teste Diminuir passou ? " + self.testDiminuir().to_s
-      puts "Teste Multiplicar passou ? " + self.testMultiplicar().to_s
+    def start_test()
+      puts "Teste da validação (As variáveis são numeros?): " + self.test_valid().to_s
+      puts "Teste soma passou ? " + self.test_soma().to_s
+      puts "Teste Divisão passou ? " + self.test_divisao().to_s
+      puts "Teste Diminuir passou ? " + self.test_diminuir().to_s
+      puts "Teste Multiplicar passou ? " + self.test_multiplicar().to_s
     end
 
-    def testSoma()
+    def test_soma()
       obj = Somar.new
       return obj.calculate(1,1) == 2 ? true : false
     end
 
-    def testDivisao()
+    def test_divisao()
       obj = Dividir.new
       return obj.calculate(12,6) == 2 ? true : false 
     end
 
-    def testDiminuir()
+    def test_diminuir()
       obj = Diminuir.new
       return obj.calculate(9,6) == 3 ? true : false 
     end
 
-    def testMultiplicar()
+    def test_multiplicar()
       obj = Multiplicar.new
       return obj.calculate(9,9) == 81 ? true : false 
     end
 
-    def testValid()
+    def test_valid()
       obj = ValidNumber.new
       return obj.valid("a",9).is_a? String   
     end
